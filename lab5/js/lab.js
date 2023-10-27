8b19f5a1e1c57d81f9740c22c60b16221efb3480
pmchoe <pmchoe@ucsc.edu>
2023-10-26 20:29:20 -0700

    finished lab 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,19 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// lab.js - Car model or something idk
+// Author: Philip Choe
+// Date: 10/24/23
 
-// Constants
+// defining variables
+make = "Tesla";
+model = "Model 3";
+color = "white";
+year = 2022;
 
-// Functions
+// calculate
+age = 2023 - year;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
