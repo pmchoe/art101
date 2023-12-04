042ef1dce00093a0795747cfed11c90d43733347
pmchoe <pmchoe@ucsc.edu>
2023-12-04 08:58:34 -0800

    idk why but i keep making this pokedex better

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -7,12 +7,7 @@ Author: Philip Choe
 Date: 12/3/23
 */
 
-// function to capitalize first letter of a string
-function capitalizeFirstLetter() {
-    return str.charAt(0).toUpperCase() + structuredClone.slice(1);
-}
-
-
+// base api url without name/id at end
 const pokemonEndpoint = "https://pokeapi.co/api/v2/pokemon/";
 
 // adds click functionality to button
@@ -33,32 +28,39 @@ $("#activate").click(function() {
         dataType : "json",
         // What do we do when the api call is successful
         //   all the action goes in here
-        success: function(data) {
-            // do stuff
-            console.log(data);
-
-            // extract relevant info from pokemon data
-            const pokemonName = data.name;
-            const pokemonTypes = data.types.map(typeObj => typeObj.type.name);
-            const pokemonAbilities = data.abilities.map(ability => ability.ability.name);
-            const pokemonHeightInFeet = Math.floor(data.height * 3.937007874 / 12);
-            const pokemonHeightRemainderInInches = Math.floor(data.height * 3.947007874 % 12);
-            const pokemonWeight = data.weight;
-
-            // print pokemon info in output div
-            $("#output").append(`
-            <p>Name: ${pokemonName}</p>
-            <p>Type(s): ${pokemonTypes.join(', ')}</p>
-            <p>Abilities: ${pokemonAbilities.join(', ')}</p>
-            <p>Height: ${pokemonHeightInFeet}' ${pokemonHeightRemainderInInches}"</p>
-            <p>Weight: ${pokemonWeight} lbs</p>
-            `);
-        },  
+        success: ajaxSuccess,
         // What we do if the api call fails
-        error: function (jqXHR, textStatus, errorThrown) { 
-            // do stuff
-            console.log("This pokemon does not exist, or");
-            console.log("Error:", textStatus, errorThrown);
-        }
+        error: ajaxError
     });
+
+    // function for api success
+    function ajaxSuccess(data) {
+        // do stuff
+        console.log(data);
+
+        // extract relevant info from pokemon data
+        const pokemonSpriteURL = data.sprites.front_default;
+        const pokemonName = data.name;
+        const pokemonTypes = data.types.map(typeObj => typeObj.type.name);
+        const pokemonAbilities = data.abilities.map(ability => ability.ability.name);
+        const pokemonHeightInFeet = Math.floor(data.height * 3.937007874 / 12);
+        const pokemonHeightRemainderInInches = Math.floor(data.height * 3.947007874 % 12);
+        const pokemonWeight = data.weight;
+
+        // print pokemon info in output div
+        $("#output").append(`
+        <p>Name: ${pokemonName}</p>
+        <img src="${pokemonSpriteURL}" alt="${pokemonName}"/>
+        <p>Type(s): ${pokemonTypes.join(', ')}</p>
+        <p>Abilities: ${pokemonAbilities.join(', ')}</p>
+        <p>Height: ${pokemonHeightInFeet}' ${pokemonHeightRemainderInInches}"</p>
+        <p>Weight: ${pokemonWeight} lbs</p>
+        `);
+    }
+
+    function ajaxError(data) {
+        // do stuff
+        console.log("This pokemon does not exist, or");
+        console.log("Error:", textStatus, errorThrown);
+    }
 });
