b27a5822ffb651a3a0e1c7d8113300208019e780
pmchoe <pmchoe@ucsc.edu>
2023-12-07 20:26:13 -0800

    finished lab16

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -10,33 +10,26 @@ Date: 12/5/23
 const endpoint = "https://xkcd.com/2863/info.0.json";
 const endpoint_latest = "https://xkcd.com/info.0.json";
 
-var comicObj = {
-    
-};
-
 
 // Using the core $.ajax() method
 $.ajax({
     // The URL for the request (from the api docs)
     url: endpoint,
-    // The data to send (will be converted to a query string)
-    data: { 
-            // here is where any data required by the api 
-            //   goes (check the api docs)
-            id: 123,
-            api_key: "blahblahblah",
-          },
     // Whether this is a POST or GET request
     type: "GET",
     // The type of data we expect back
     dataType : "json",
     // What do we do when the api call is successful
     //   all the action goes in here
-    success: function(data) {
+    success: function(comicObj) {
         // do stuff
-        console.log(data);
+        console.log(comicObj);
+        // get comic title
+        $("#output").append("<p>" + comicObj.title + "</p>");
+        // get comic img
+        $("#output").append("<img id='img' src=" + comicObj.img + " alt=" + comicObj.alt + ">");
     },
-    // What we do if the api call fails
+    // What we do if the api call fails 
     error: function (jqXHR, textStatus, errorThrown) { 
         // do stuff
         console.log("Error:", textStatus, errorThrown);
